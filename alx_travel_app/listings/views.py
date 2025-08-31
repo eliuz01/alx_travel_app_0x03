7b74b742d689516918ce80125277ff81e758b934
ELIUZ <osongoeliuz14@gmail.com>
2025-08-31 07:47:51 +0300

    created celery.py

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -2,6 +2,11 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Listing, Booking
 from .serializers import ListingSerializer, BookingSerializer
+from .tasks import send_booking_confirmation
+
+def perform_create(self, serializer):
+    booking = serializer.save()
+    send_booking_confirmation.delay(booking.user.email, str(booking))
 
 
 class ListingViewSet(viewsets.ModelViewSet):
